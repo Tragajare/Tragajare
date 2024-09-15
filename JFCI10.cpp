d8e29b1546d381a4fd896d223fa5a083b492221c
Juan Ishino <142410260+Tragajare@users.noreply.github.com>
2024-09-15 14:40:20 -0300

    Add files via upload
    programa 10 - definir valor da varavel usando scanf

---
+++ JFCI10.cpp
@@ -0,0 +1,15 @@
+//JFCI10 - definir valor da varavel usando scanf
+//Juan Fernando Coucolis Ishino 8/3/2023
+
+#include <iostream>
+main()
+{ int Num;
+
+printf("digite um numnero inteiro: ");
+scanf("%d", &Num);
+printf("foi digitado: %d", Num);
+
+/* & na frente (grudado) indica o endereco da variavel seguinte
+scanf funcao de scaneamento*/ 
+
+}
