79222892da2497d95cece7b96e1a28358b7cf35e
Juan Ishino <142410260+Tragajare@users.noreply.github.com>
2024-09-15 14:44:23 -0300

    Add files via upload
    Programa 11 - definir valor da variavel float usando scanf

---
+++ JFCI11.cpp
@@ -0,0 +1,16 @@
+//JFCI11 - definir valor da variavel float usando scanf
+//Juan Fernando Coucolis Ishino 8/3/2023
+#include <iostream>
+main()
+{
+float num;
+
+printf("Digite um numero: ");
+scanf("%f", &num);
+
+printf("o numero digitado foi: %.1f\n", num);
+
+system ("pause");
+
+
+}
