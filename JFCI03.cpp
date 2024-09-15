d200859190c45c51717f43af245a7a6909a758b2
Juan Ishino <142410260+Tragajare@users.noreply.github.com>
2024-09-14 23:17:19 -0300

    Add files via upload
    Programa 3 - Desenho com printf

---
+++ JFCI03.cpp
@@ -0,0 +1,15 @@
+//JFCI - Fazer um retangulo com seu usando printf
+//Juan Fernando Coucolis Ishino 3/3/2023
+
+//#include <stdio.h>
+#include <iostream>
+
+main()
+{
+printf(" +--------------------------+ \n");
+printf(" |      Juan F C Ishino     | \n");
+printf(" |          PUC- SP         | \n");
+printf(" +--------------------------+ \n");
+system("pause");
+
+}
