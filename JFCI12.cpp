91f6dcf2a4d029e2b4f471e5d1d76ae4c6311a76
Juan Ishino <142410260+Tragajare@users.noreply.github.com>
2024-09-15 14:51:04 -0300

    Add files via upload
    Programa 12

---
+++ JFCI12.cpp
@@ -0,0 +1,17 @@
+//JFCI12 -  entrar com 3 numeros inteiros
+//Juan Fernando Coucolis Ishino 10/3/2023
+
+#include <iostream>
+main()
+{
+int n1,n2,n3;
+
+printf("digite 3 numeros inteiros, separados por espaco: ");
+scanf("%d %d %d", &n1, &n2, &n3);
+
+printf("os numeros digitados foram: %d %d %d\n", n1,n2,n3);
+
+system("pause");
+}
+
+
