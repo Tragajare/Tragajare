b3892c97bd9d21b887df5e73f590d7d9ad92cf23
Juan Ishino <142410260+Tragajare@users.noreply.github.com>
2024-09-14 23:26:27 -0300

    Add files via upload
    Programa 4 - Definição de variáveis int(inteiro)

---
+++ JFCI04.cpp
@@ -0,0 +1,23 @@
+//JFCI - Definicao das variaveis int(inteiro)
+//Juan Fernando Coucolis Ishino 3/3/2023
+
+// (= é uma atribuição) (== é igualdade)
+// %d serve para dizer onde a variavel deve entrar, somente para numeros INTEIROS
+// /n indica quantas linhas vai pular
+// /t significa tabulação
+
+#include<stdio.h>
+//#include<iostream>
+
+main()
+{
+int X, S;
+
+X = 122;
+S = 9999993;
+
+printf("A variavel N contem:%d\n", X);
+printf("A variavel S contem:%d\n\n", S);
+printf("%d em N\t  %d em S\n\n", X, S);
+
+}
