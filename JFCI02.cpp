055a777c56963fb4005f40d9b4c203cafedaa89d
Juan Ishino <142410260+Tragajare@users.noreply.github.com>
2024-09-14 23:12:49 -0300

    Add files via upload

---
+++ JFCI02.cpp
@@ -0,0 +1,16 @@
+//JFCI - Fazer um desenho com printf
+//Juan Fernando Coucolis Ishino 3/3/2023
+
+#include <stdio.h>
+main()
+{
+printf("       *       \n");
+printf("      ***      \n");
+printf("     *****     \n");
+printf("    *******    \n");
+printf("   **juan***   \n");
+printf("  ***********  \n");
+printf("      ***      \n");
+printf("      ***      \n");
+printf("      ***      \n");
+printf("     *****     \n");
